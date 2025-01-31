1203d08e60bd4e07df92a7377f8c675d94b0cfa0
tasuku-lab280 <56028684+tasuku-lab280@users.noreply.github.com>
2025-01-31 13:52:26 +0000

    add: user model 追加

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+end
